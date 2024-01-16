5ccacd9bcb50f1549d3336d807e504d323a5e5bf
0xsuid <45615666+0xsuid@users.noreply.github.com>
2024-01-16 18:51:50 +0100

    Add 11ty plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,16 @@
-const tailwind = require("tailwindcss");
+const { DateTime } = require("luxon");
+
 const postCss = require("postcss");
+const tailwind = require("tailwindcss");
 const autoPrefixer = require("autoprefixer");
 const cssNano = require("cssnano");
+
+const pluginBundle = require("@11ty/eleventy-plugin-bundle");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginNavigation = require("@11ty/eleventy-navigation");
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({
@@ -9,6 +18,15 @@ module.exports = function (eleventyConfig) {
     });
 
     eleventyConfig.addWatchTarget("./public/css/");
+
+    // Official plugins
+	eleventyConfig.addPlugin(pluginRss);
+	eleventyConfig.addPlugin(pluginSyntaxHighlight, {
+		preAttributes: { tabindex: 0 }
+	});
+	eleventyConfig.addPlugin(pluginNavigation);
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+	eleventyConfig.addPlugin(pluginBundle);
 
     eleventyConfig.addNunjucksAsyncFilter("postcss", (code, done) => {
         postCss([
@@ -24,6 +42,11 @@ module.exports = function (eleventyConfig) {
                 (e) => done(e, null)
             );
     });
+
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+	});
 
     eleventyConfig.addFilter("randomItem", (arr) => {
         arr.sort(() => {
