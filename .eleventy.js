56c947b5dc93b6254a47784dff3b3d54ea3bcde7
0xsuid <45615666+0xsuid@users.noreply.github.com>
2024-02-18 17:42:15 +0100

    Update sitemap - datetime UTC format

--- .eleventy.js
+++ .eleventy.js
@@ -72,9 +72,9 @@ module.exports = function (eleventyConfig) {
             );
     });
 
-    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    eleventyConfig.addFilter('htmlDateTimeString', (dateObj) => {
 		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toISO();
 	});
 
     eleventyConfig.addFilter("fullUrl", function (url = '', base = eleventyConfig.globalData.baseUrl) {
